0e0a3772d8653db539b6c43ea3125a3dd49004b2
Jacob Slunga <jacobslunga@Jacobs-MacBook-Air.local>
2023-11-15 21:25:39 +0100

    Update colors.lua to use default colorscheme and color for cursor line number

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,18 +1,20 @@
 function ColorMyPencils()
-  require("colorbuddy").colorscheme "gruvbuddy"
-  require("colorizer").setup()
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+ -- require("colorbuddy").colorscheme "gruvbuddy"
+ -- require("colorizer").setup()
 
-  local Group = require("colorbuddy.group").Group
-  local c = require("colorbuddy.color").colors
-  local styles = require("colorbuddy.style").styles
+ -- local Group = require("colorbuddy.group").Group
+ -- local c = require("colorbuddy.color").colors
+ -- local styles = require("colorbuddy.style").styles
 
-  Group.new("Normal", c.white, c.none)
-  Group.new("GoTestSuccess", c.green, nil, styles.bold)
-  Group.new("GoTestFail", c.red, nil, styles.bold)
+ -- Group.new("Normal", c.white, c.none)
+ -- Group.new("GoTestSuccess", c.green, nil, styles.bold)
+ -- Group.new("GoTestFail", c.red, nil, styles.bold)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, 'CursorLineNr', {fg='yellow', bg='NONE', bold=true})
+  vim.api.nvim_set_hl(0, 'CursorLineNr', {fg='white', bg='NONE', bold=true})
   vim.api.nvim_set_hl(0, 'SignColumn', {bg='NONE'})
   vim.api.nvim_set_hl(0, 'LineNr', {fg='#A9A9A9', bg='NONE'})
 end
